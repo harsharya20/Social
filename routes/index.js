31ff8ebd06147d710d2d3652f8af83010ebba749
Harsh <harsharya7002@gmail.com>
2021-05-05 22:26:54 +0530

    setting up conroller and routes js files

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,8 @@ const homeController=require('../controller/home_controller');
 console.log('router is loaded');
 
 router.get('/',homeController.home);
+router.use('/users',require('./users'));
+router.use('/post',require('./post'));
+//for any furhter rotes,access from here
+//router.use('/routername',require('./routerfile));
 module.exports=router;
