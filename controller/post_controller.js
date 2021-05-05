e245c7395bcf3593735c1a1c49e2a436e872596a
Harsh <harsharya7002@gmail.com>
2021-05-05 22:54:10 +0530

    creating ejs in views

--- controller/post_controller.js
+++ controller/post_controller.js
@@ -1,3 +1,5 @@
 module.exports.posts=function(req,res){
-    return res.end('<h1>User Post</h1>');
+    return res.render('post',{
+        title: "Posts"
+    });
 }
--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const port=3000;
 
 //use express router
 app.use('/',require('./routes'));
+
+//for setting up view engine
+app.set('view engine','ejs');
+app.set('views','./views');
+
 app.listen(port,function(err){
     if(err){
         console.log(`Error in running the server: ${err}`);
