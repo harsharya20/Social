1cb44863a9ac70c106872a511dbadccb4ff5df0f
Harsh <harsharya7002@gmail.com>
2021-05-09 20:09:19 +0530

    creating sign-up

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -1,4 +1,6 @@
-module.exports.profile=function(req,res){
+ const User =require('../models/user') ;
+  
+  module.exports.profile=function(req,res){
     return res.render('user_profile',{
         title: "User"
    });
@@ -17,7 +19,23 @@ module.exports.signIn =function(req,res){
 
 //grt the sign up data
 module.exports.create =function(req,res){
-    //LAter
+    if(req.body.password !=req.body.confirm_password){
+        return res.redirect('back');
+    }
+    User.findOne({email:req.body.email}, function(err, user){
+        if(err){console.log('error in finding user in signing up'); return}
+
+        if(!user){
+            User.create(req.body,function(err,user){
+                if(err){console.log('error in creating user while signing up'); return}
+
+                return res.redirect('/users/sign-in');
+
+            })
+        }else {
+            return res.redirect('back');
+        }
+    });
 }
 //sign-in and create session for the user
 module.exports.createSession =function(req,res){
