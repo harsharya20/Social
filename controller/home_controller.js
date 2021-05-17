a1db9f5d77f834583237e62e467712c1722df99f
Harsh <harsharya7002@gmail.com>
2021-05-17 16:09:01 +0530

    update the profile of the user

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,4 +1,5 @@
 const Post= require('../models/post');
+const User =require('../models/user');
 
 
 module.exports.home=function(req,res){
@@ -22,9 +23,15 @@ Post.find({})
   }
 })
 .exec(function(err,posts){
+  User.find({},function(err,users){
     return res.render('home',{
-          title: "Codeial | Home",
-            posts : posts
-      });
+      title: "Codeial | Home",
+        posts : posts,
+        all_users: users
+  });
+  });
+
+
+   
 })
 }
