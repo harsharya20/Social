d795da66cd3c2fb69da370c14b27bc1fe29575d5
Harsh <harsharya7002@gmail.com>
2021-05-17 14:43:58 +0530

    distributing code into partials

--- routes/comments.js
+++ routes/comments.js
@@ -7,5 +7,4 @@ router.post('/create',passport.checkAuthentication,commentsController.create);
 router.get('/destroy/:id',passport.checkAuthentication,commentsController.destroy);
 
 
-
 module.exports=router;
