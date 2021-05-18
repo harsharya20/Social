2850ce837333ca3bf1d48687599e649ce2c571ac
Harsh <harsharya7002@gmail.com>
2021-05-18 17:09:42 +0530

    converting to async await i.e function contain some asynchronous statement and u need to wait once the statement gets executed then move on to the next statement

--- controller/comments_controller.js
+++ controller/comments_controller.js
@@ -1,40 +1,48 @@
 const Comment =require('../models/comment');
 const Post = require('../models/post');
 
-module.exports.create= function(req,res){
-    Post.findById(req.body.post, function(err, post){
+module.exports.create= async function(req,res){
+
+    try{
+        let post =await Post. findById(req.body.post);
+
         if(post){
-            Comment.create({
+           let comment = await Comment.create({
                 content : req.body.content,
                 post:req.body.post,
                 user: req.user._id
-            }, function(err,comment){
-                //handle error
+           });
 
                 post.comments.push(comment);
                 post.save();
 
                 res.redirect('/');
-            });
+            }
         }
-    });
+    catch(err){
+        console.log('Error', err);
+        return;
+    }
 }
 
-module.exports.destroy= function(req,res){
-    Comment.findById(req.params.id, function(err,comment){
-        if(comment.user == req.user.id){
-             let postId= comment.post;
+module.exports.destroy=async function(req,res){
+ try{
+    let comment= await  Comment.findById(req.params.id);
+    if(comment.user == req.user.id){
+         let postId= comment.post;
 
-             comment.remove();
+         comment.remove();
 
-             Post.findByIdAndUpdate(postId, {$pull: {comments: req.params.id}}, function(err,post)//removing id of the comment from the list of comments
-             {
-                 return res.redirect('back');
-                 
-        });
-    }else{
-        return res.redirect('back');
-    }
-});
+        let post= Post.findByIdAndUpdate(postId, {$pull: {comments: req.params.id}});//removing id of the comment from the list of comments
+             return res.redirect('back');
+}else{
+    return res.redirect('back');
+}
+ }
+ catch(err){
+    console.log('Error', err);
+    return;
+ }
+  
 }
 
--- controller/home_controller.js
+++ controller/home_controller.js
@@ -2,36 +2,27 @@ const Post= require('../models/post');
 const User =require('../models/user');
 
 
-module.exports.home=function(req,res){
- //console.log(req.cookies);
- //res.cookie('user_id',25);
-
- //Post.find({}, function(err,posts){ // return all the posts
-  //   return res.render('home',{
-    //      title: "Codeial | Home",
-   //       posts : posts
- //    });
-// });
-   
-//Populate the user of each post
-Post.find({})
+module.exports.home= async function(req,res){
+  try{
+        //Populate the user of each post
+let posts= await Post.find({})
 .populate('user')
 .populate({
   path:'comments',
   populate:{
     path : 'user'
   }
-})
-.exec(function(err,posts){
-  User.find({},function(err,users){
-    return res.render('home',{
-      title: "Codeial | Home",
-        posts : posts,
-        all_users: users
-  });
-  });
+});
 
-
-   
-})
+let users= await User.find({});
+return res.render('home',{
+  title: "Codeial | Home",
+    posts : posts,
+    all_users: users
+});
+  } 
+  catch(err){
+    console.log('Error',err);
+    return;
+  }
 }
