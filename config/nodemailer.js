123db6fac00066ddfaf8a35e18c53eff6f9d409e
Harsh <harsharya7002@gmail.com>
2021-05-23 20:10:54 +0530

    setting up nodemailer

---
+++ config/nodemailer.js
@@ -0,0 +1,37 @@
+const nodemailer = require('nodemailer');
+const ejs = require('ejs');
+const path = require('path');
+
+// this is part which sends email
+let transporter = nodemailer.createTransport({
+    service : 'gmail',
+    host: 'smtp.gmail.com',
+    port : 587,
+    secure: false,
+    auth : {
+        user : 'harya9263',
+        pass : 'harsharya123'
+    }
+});
+  
+     //defines whenever I m going to send HTML request file would be placed inside views   // relativePath :from where the mail is being send
+let renderTemplate = (data , relativePath ) => {
+  let mailHTML;
+  ejs.renderFile(
+       path.join(__dirname, '../views/mailers' , relativePath),
+       data,
+       function(err, template){
+           if(err){console.log('error in rendering template'); return;}
+
+           mailHTML = template;
+
+       }
+  )
+  return mailHTML;
+}
+
+
+module.exports = {
+    transporter : transporter,
+    renderTemplate = renderTemplate
+}
