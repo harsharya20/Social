cf1c5c66a224b09b42021e8850e46b749a1a9226
Harsh <harsharya7002@gmail.com>
2021-05-24 18:38:04 +0530

    Sending HTML template email

--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -2,22 +2,21 @@ const nodeMailer = require('../config/nodemailer');
 
 // this is another way of exporting a method
 exports.newComment = (comment) => {
-    console.log('Inside newComment mailer');
+    let htmlString = nodeMailer.renderTemplate({comment : comment},'/comments/new_comment.ejs');
 
     //for sending mail
     nodeMailer.transporter.sendMail({
-      from: 'harya9263@gmail.com', //isme kon sa mail daalte h? /// jo nodemailer me diya ho isme dusrA MAIL DAAL KE DEKHU? try kr
+      from: 'harshoffice@gmail.com', 
       to: comment.user.email,
       subject: 'New comment published!', // google account pr jaana 
-      html: '<h1>Yup , Your comment is now published!</h1>'
+      html: htmlString
     }, (err, info) => {
         if(err){
             console.log('error in sending mail', err);
             return; 
         }
 
-        console.log('Message sent', info);// apne gmail se 2 step verification on kr rkhi h ? ye kya hota h yeh ek authentication hoti h gmail ki jisse apka account secure rhe kaise krte h
-        // apne google account pr jao jo email me diya h uske
+        console.log('Message sent', info);
         return;
     }
     );
