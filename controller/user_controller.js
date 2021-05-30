f669b352fb8253f8d33d96aab6ba250b3aa27870
Harsh <harsharya7002@gmail.com>
2021-05-30 18:28:43 +0530

    'changes'

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -71,7 +71,7 @@ module.exports.signIn =function(req,res){
     if(req.isAuthenticated()){
       return  res.redirect('/users/profile');
     }
-    return res.render('user_sign_up',{
+    return res.render('user_sign_in',{
         title:"Codial | Sign In"
  })
 }
