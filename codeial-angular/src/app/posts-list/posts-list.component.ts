3331612371d7a4f4da3e15f0af27f2ec5363a567
Harsh <harsharya7002@gmail.com>
2021-06-01 15:50:31 +0530

    setting up cors for giving access to port 4200

--- codeial-angular/src/app/posts-list/posts-list.component.ts
+++ codeial-angular/src/app/posts-list/posts-list.component.ts
@@ -1,18 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import { PostsListService } from '../posts-list.service';
 @Component({
   selector: 'app-posts-list',
   templateUrl: './posts-list.component.html',
   styleUrls: ['./posts-list.component.scss']
 })
 export class PostsListComponent implements OnInit {
+  posts= [];
 
-  constructor() { }
+  constructor(private postsListService: PostsListService) { }
 
   ngOnInit(): void {
   }
 
   getPosts(): void{
-    
+   this.postsListService.getPosts().subscribe((data) => (this.posts = data.posts));
   }
 }
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const env = require('./config/environment');
 const logger = require('morgan');
+const cors = require('cors');
 
 const cookieParser=require('cookie-parser');
 const app=express();
@@ -28,6 +29,8 @@ const chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
 chatServer.listen(5000);
 console.log('chat server is listening on port 5000');
 const path = require('path');
+
+app.use(cors());
 
 if(env.name == 'development'){
 app.use(sassMiddleware({
