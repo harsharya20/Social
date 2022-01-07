e1cbb150546f4faf693a51c360793025dda5bb69
Harsh <harsharya7002@gmail.com>
2022-01-07 19:37:20 +0530

    chat engine fix

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express =require('express');
 const router=express.Router();
 const homeController=require('../controller/home_controller');
-
+const signinController=require('../controller/user_controller');
 
 console.log('router is loaded');
+
 
 router.get('/',homeController.home);
 router.use('/users',require('./users'));
